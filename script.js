ff915ef28adc4a3d4dfa88eafbd29407bb89c4cd
thanusrithiruvaipati-oss <thanusrithiruvaipati@gmail.com>
2026-06-13 16:53:02 +0530

    added search bar and categories

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const searchBar = document.getElementById("searchBar");
+const filterButtons = document.querySelectorAll(".filter-btn");
+const items = document.querySelectorAll(".item");
 const galleryimages = document.querySelectorAll(".gallery img");
 const lightbox = document.querySelector(".lightbox");
 const lightboximage = document.querySelector(".lightbox-image");
@@ -5,6 +8,47 @@ const closebtn = document.querySelector(".close");
 const nextbtn = document.querySelector(".next");
 const prevbtn = document.querySelector(".prev");
 let Cindex = 0;
+
+
+searchBar.addEventListener("keyup", () => {
+    const searchText = searchBar.value.toLowerCase();
+
+    items.forEach(item => {
+        const title = item.querySelector("h3").textContent.toLowerCase();
+
+        if(title.includes(searchText)){
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+});
+
+filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        filterButtons.forEach(btn =>
+            btn.classList.remove("active")
+        );
+
+        button.classList.add("active");
+
+        const category = button.dataset.category;
+
+        items.forEach(item => {
+
+            if(
+                category === "all" ||
+                item.dataset.category === category
+            ){
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+
+        });
+    });
+});
 
 galleryimages.forEach((image, index) => {
     image.addEventListener("click", () => {
